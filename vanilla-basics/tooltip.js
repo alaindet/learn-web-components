b4fb91130f802bd76887825b06fe68eeb19526bc
Alain D'Ettorre <alain.det@gmail.com>
2020-11-12 00:04:22 +0100

    Create first shadowRoot

--- vanilla-basics/tooltip.js
+++ vanilla-basics/tooltip.js
@@ -4,27 +4,44 @@ class MyTooltip extends HTMLElement {
     super();
     this._tooltipContainer;
     this._tooltipText = 'Default tooltip text';
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          color: #6060cc;
+          border: 1px dashed #8080ff;
+        }
+        div {
+          background-color: black;
+          color: white;
+          position: absolute;
+          z-index: 2;
+        }
+      </style>
+      <slot>Default tooltip</slot>
+      <span>(?)</span>
+    `;
   }
 
   connectedCallback() {
-    const tooltipIcon = document.createElement('span');
-    tooltipIcon.textContent = ' (?)';
     if (this.hasAttribute('text')) {
       this._tooltipText = this.getAttribute('text');
     }
+    const tooltipIcon = this.shadowRoot.querySelector('span');
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative';
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
-    this.appendChild(this._tooltipContainer);
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
