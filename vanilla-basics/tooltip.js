4bb548fca141803783a8583933b5bf81f908a31f
Alain D'Ettorre <alain.det@gmail.com>
2020-11-15 19:10:37 +0100

    Use attributeChangedCallback and disconnectedCallback lifecycle hooks

--- vanilla-basics/tooltip.js
+++ vanilla-basics/tooltip.js
@@ -1,8 +1,13 @@
 class MyTooltip extends HTMLElement {
 
+  static get observedAttributes() {
+    return ['text'];
+  }
+
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipIcon;
     this._tooltipText = 'Default tooltip text';
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
@@ -65,11 +70,24 @@ class MyTooltip extends HTMLElement {
     if (this.hasAttribute('text')) {
       this._tooltipText = this.getAttribute('text');
     }
-    const tooltipIcon = this.shadowRoot.querySelector('span');
-    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    this.shadowRoot.appendChild(tooltipIcon);
+    this._tooltipIcon = this.shadowRoot.querySelector('span');
+    this._tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.style.position = 'relative';
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+    if (name === 'text') {
+      this._tooltipText = newValue;
+    }
+  }
+
+  disconnectedCallback() {
+    this._tooltipIcon.removeEventListener('mouseenter');
+    this._tooltipIcon.removeEventListener('mouseleave');
   }
 
   _showTooltip() {
