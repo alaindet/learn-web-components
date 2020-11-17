6d9f10db2d38e0b943de36cdce004035393ab767
Alain D'Ettorre <alain.det@gmail.com>
2020-11-17 08:30:29 +0100

    Use "slotchange" browser event

--- vanilla-advanced/modal.js
+++ vanilla-advanced/modal.js
@@ -89,6 +89,13 @@ class MyModal extends HTMLElement {
         </footer>
       </div>
     `;
+    const slots = this.shadowRoot.querySelectorAll('slot');
+    slots[1].addEventListener(
+      'slotchange',
+      (event) => {
+        console.dir(slots[1].assignedNodes());
+      }
+    );
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
