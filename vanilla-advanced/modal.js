e1d8fb5203af729420b2590e8cc85ddaee5c21cd
Alain D'Ettorre <alain.det@gmail.com>
2020-11-17 07:48:44 +0100

    Named slots and CSS-only modal open

--- vanilla-advanced/modal.js
+++ vanilla-advanced/modal.js
@@ -6,9 +6,15 @@ class MyModal extends HTMLElement {
 
   constructor() {
     super();
+    this.isOpen = false;
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
       <style>
+        :host([opened]) #backdrop,
+        :host([opened]) #modal {
+          opacity: 1;
+          pointer-events: all;
+        }
         #backdrop {
           position: fixed;
           top: 0;
@@ -17,6 +23,7 @@ class MyModal extends HTMLElement {
           left: 0;
           background-color: rgba(0,0,0,0.5);
           z-index: 2;
+          transition: 0.2s opacity ease-in-out;
 
           /* Hide  by default */
           pointer-events: none;
@@ -36,6 +43,7 @@ class MyModal extends HTMLElement {
           border-radius: 0.5rem;
           display: flex;
           flex-direction: column;
+          transition: 0.2s opacity ease-in-out;
 
           /* Hide  by default */
           pointer-events: none;
@@ -46,7 +54,7 @@ class MyModal extends HTMLElement {
           padding-bottom: 1rem;
           margin-bottom: 1rem;
         }
-        #modal-header h1 {
+        ::slotted(h1) {
           margin: 0;
           font-size: 1.5rem;
         }
@@ -68,7 +76,9 @@ class MyModal extends HTMLElement {
 
       <div id="modal">
         <header id="modal-header">
-          <h1>Are you sure?</h1>
+          <slot name="title">
+            <h1>Default title</h1>
+          </slot>
         </header>
         <section id="modal-body">
           <slot></slot>
@@ -82,17 +92,12 @@ class MyModal extends HTMLElement {
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (oldValue === newValue) {
-      return;
-    }
-    if (name === 'opened') {
-      if (this.hasAttribute('opened')) {
-        this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-        this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'initial';
-        this.shadowRoot.querySelector('#modal').style.opacity = 1;
-        this.shadowRoot.querySelector('#modal').style.pointerEvents = 'initial';
-      }
-    }
+    this.isOpen = this.hasAttribute('opened');
+  }
+
+  open() {
+    this.setAttribute('opened', '');
+    this.isOpen = true;
   }
 }
 
