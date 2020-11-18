3af1cc97a2e24c0c1c722ac3fba4494f8f878c77
Alain D'Ettorre <alain.det@gmail.com>
2020-11-18 09:30:01 +0100

    Create <my-side-drawer>

--- stencil-basics/src/components.d.ts
+++ stencil-basics/src/components.d.ts
@@ -20,6 +20,8 @@ export namespace Components {
          */
         "middle": string;
     }
+    interface MySideDrawer {
+    }
 }
 declare global {
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
@@ -28,8 +30,15 @@ declare global {
         prototype: HTMLMyComponentElement;
         new (): HTMLMyComponentElement;
     };
+    interface HTMLMySideDrawerElement extends Components.MySideDrawer, HTMLStencilElement {
+    }
+    var HTMLMySideDrawerElement: {
+        prototype: HTMLMySideDrawerElement;
+        new (): HTMLMySideDrawerElement;
+    };
     interface HTMLElementTagNameMap {
         "my-component": HTMLMyComponentElement;
+        "my-side-drawer": HTMLMySideDrawerElement;
     }
 }
 declare namespace LocalJSX {
@@ -47,8 +56,11 @@ declare namespace LocalJSX {
          */
         "middle"?: string;
     }
+    interface MySideDrawer {
+    }
     interface IntrinsicElements {
         "my-component": MyComponent;
+        "my-side-drawer": MySideDrawer;
     }
 }
 export { LocalJSX as JSX };
@@ -56,6 +68,7 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "my-side-drawer": LocalJSX.MySideDrawer & JSXBase.HTMLAttributes<HTMLMySideDrawerElement>;
         }
     }
 }
